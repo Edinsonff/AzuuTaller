0cb236baeb008484bf6760538e3ea268e5e483f0
Edinson Rodriguez <edinson.rodriguez@utp.edu.co>
2024-09-30 10:06:41 -0500

    feature: Se implementaron la funcionalidad de cambio de idioma

--- public/prueba.js
+++ public/prueba.js
@@ -1,12 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const carouselItems = document.querySelectorAll('.carousel-item');
+
+    const carouselItems = document.querySelectorAll('.carousel-itme');
     let currentIndex = 0;
+    let firstThemeChange = true;
+
+    //funcion para actualizar el carrusel
 
     const updateCarousel = (index) => {
         carouselItems[currentIndex].classList.remove('active', 'fade-in');
         currentIndex = index;
         carouselItems[currentIndex].classList.add('active', 'fade-in');
-    };
+    }
+
+    //controles del carrusel
 
     const prevButton = document.getElementById('prev');
     const nextButton = document.getElementById('next');
@@ -15,13 +21,14 @@ document.addEventListener('DOMContentLoaded', () => {
         prevButton.addEventListener('click', () => {
             const newIndex = (currentIndex === 0) ? carouselItems.length - 1 : currentIndex - 1;
             updateCarousel(newIndex);
-        });
+        })
 
         nextButton.addEventListener('click', () => {
             const newIndex = (currentIndex === carouselItems.length - 1) ? 0 : currentIndex + 1;
             updateCarousel(newIndex);
-        });
+        })
     }
+
 
     const htmlElement = document.querySelector('html');
     const toggleButtons = document.querySelectorAll('#toggle, #toggle-sm');
@@ -30,111 +37,112 @@ document.addEventListener('DOMContentLoaded', () => {
     const closeMobileMenuButton = document.getElementById('close-mobile-menu');
     const openMobileMenuButton = document.getElementById('mobile-menu-button');
 
-    const toggleDarkMode = () => {
-        // Aplicar fade-out a todos los elementos excepto el carrusel
-        const elementsToFade = document.querySelectorAll('body > *:not(.carousel-container)');
-        elementsToFade.forEach(element => {
-            element.classList.add('fade-out');
-        });
 
+    const toggleDarkMode = () => {
+        if (firstThemeChange) {
+            bodyElement.classList.add('disable-animations')
+        }
+
+        document.body.classList.add('fade-out');
         setTimeout(() => {
             htmlElement.classList.toggle('dark');
-            
-            elementsToFade.forEach(element => {
-                element.classList.remove('fade-out');
-                element.classList.add('fade-in');
-            });
+            document.body.classList.remove('fade-out');
+            document.body.classList.add('fade-in');
+            if (firstThemeChange) {
+                setTimeout(() => {
+                    bodyElement.classList.remove('disable-animations');
+                    firstThemeChange = false;
+                }, 0);
+            }
         }, 500);
-    };
+    }
 
     toggleButtons.forEach(button => button.addEventListener('click', toggleDarkMode));
 
-    // Menú móvil
     const openMobileMenu = () => {
         mobileMenu.classList.remove('hidden');
         document.body.classList.add('modal-active');
-        document.body.style.overflow = 'hidden';
-    };
+        document.body.style.overflow = 'hidden'
+    }
 
     const closeMobileMenu = () => {
         mobileMenu.classList.add('hidden');
         document.body.classList.remove('modal-active');
         document.body.style.overflow = 'auto';
-    };
+    }
 
     if (openMobileMenuButton && mobileMenu && closeMobileMenuButton) {
         openMobileMenuButton.addEventListener('click', openMobileMenu);
         closeMobileMenuButton.addEventListener('click', closeMobileMenu);
     }
 
-    // Funcionalidad de cambio de idioma
     const changeLanguageButton = document.getElementById('change-language');
+
     let isEnglish = false;
 
     const updateLanguage = () => {
-        console.log('Changing language...');
         const lang = isEnglish ? 'es' : 'en';
 
-        // Cambiar texto en el menú de navegación
         const nosotrosLink = document.getElementById('nosotros-link');
-        const proyectosLink = document.getElementById('proyectos-link');
+        const serviciosLink = document.getElementById('servicios-link');
 
-        // Asegúrate de que los elementos existen antes de cambiar su texto
-        if (nosotrosLink && proyectosLink) {
-            nosotrosLink.textContent = translations[lang]["nosotros-link"];
-            proyectosLink.textContent = translations[lang]["proyectos-link"];
+        if (nosotrosLink && serviciosLink) {
+            nosotrosLink.textContent = translations[lang]['nosotros'];
+            serviciosLink.textContent = translations[lang]['servicios'];
         }
 
-        carouselItems.forEach((carouselItem, index) => {
-            const carouselTitle = carouselItem.querySelector('h2');
-            const carouselSubtitle = carouselItem.querySelector('p');
-            const carouselTeam = carouselItem.querySelector('.team');
+        carouselItems.forEach((currentItem, index) => {
+            const carouselTitle = carouselItems.querySelector('h2');
+            const carouselSubtitle = carouselItems.querySelector('p');
+            const carouselTeam = carouselItems.querySelector('.team');
 
             const carouselData = translations[lang]["carousel-text"][index];
+
 
             if (Array.isArray(carouselData)) {
                 if (carouselTitle) {
                     carouselTitle.textContent = carouselData[0];
                 }
                 if (carouselSubtitle) {
-                    carouselSubtitle.textContent = carouselData[1] || ""; 
+                    carouselSubtitle.textContent = carouselData[1] || "";
                 }
-                if (carouselTeam && carouselData[2]) {
+                if (carouselTeam) {
                     carouselTeam.textContent = carouselData[2];
                 }
             } else {
+
                 if (carouselTitle) {
                     carouselTitle.textContent = carouselData;
                 }
+
                 if (carouselSubtitle) {
-                    carouselSubtitle.textContent = "";
+                    carouselSubtitle.textContent = '';
                 }
+
                 if (carouselTeam) {
-                    carouselTeam.textContent = "";
+                    carouselTeam.textContent = '';
                 }
             }
         });
 
         if (document.body.classList.contains('nosotros-page')) {
-            const aboutTitle = document.querySelector('.about-page-title');
+            const aboutTitle = document.getElementById('about-page-title');
             const aboutDescriptions = document.querySelectorAll('.about-page-description');
-            const aboutPageFooter = document.querySelector('.about-page-footer');
+            const aboutPageFooter = document.getElementById('about-page-footer');
 
-            if (aboutTitle) {
-                aboutTitle.textContent = translations[lang]["about-page-title"];
-            }
-            if (aboutDescriptions.length > 0) {
-                aboutDescriptions.forEach((el, index) => {
-                    el.textContent = translations[lang]["about-page-description"][index];
+            if (aboutTitle && aboutDescriptions && aboutPageFooter) {
+                aboutTitle.textContent = translations[lang]['about-page-title'];
+                aboutPageFooter.textContent = translations[lang]['about-page-footer'];
+
+                aboutDescriptions.forEach((description, index) => {
+                    description.textContent = translations[lang]['about-page-description'][index];
+
                 });
-            }
-            if (aboutPageFooter) {
-                aboutPageFooter.textContent = translations[lang]["about-page-footer"];
             }
         }
 
-        if (changeLanguageButton) {
-            changeLanguageButton.textContent = isEnglish ? "ENG" : "ESP";
+        if ( changeLanguageButton ) {
+            changeLanguageButton.textContent = isEnglish ? 'ENG' : 'ESP';
         }
 
         isEnglish = !isEnglish;
@@ -143,25 +151,4 @@ document.addEventListener('DOMContentLoaded', () => {
     if (changeLanguageButton) {
         changeLanguageButton.addEventListener('click', updateLanguage);
     }
-
-    const addFadeOutAnimation = (event, href) => {
-        event.preventDefault();
-        document.body.classList.add('fade-out');
-        setTimeout(() => {
-            window.location.href = href;
-        }, 500); // Debe coincidir con la duración de la animación
-    };
-
-    const nosotrosLink = document.getElementById('nosotros-link');
-    const proyectosLink = document.getElementById('proyectos-link');
-
-    if (nosotrosLink) {
-        nosotrosLink.addEventListener('click', (event) => addFadeOutAnimation(event, './Nosotros.html'));
-    }
-
-    if (proyectosLink) {
-        proyectosLink.addEventListener('click', (event) => addFadeOutAnimation(event, './index.html'));
-    }
-
-    document.body.classList.add('fade-in');
 });
---
+++ public/translations.js
@@ -0,0 +1,68 @@
+const translations = {
+    es: {
+        "nosotros-link": "Nosotros",
+        "proyectos-link": "Proyectos",
+        "carousel-text": [
+      ["Salón social", "Palacé, Cauca. Colombia (2021)", ""],
+      ["Casa Concha", "Popayán, Cauca. Colombia (2020)", ""],
+      ["Patio Vivas", "Popayán, Cauca. Colombia (2022)", ""],
+      ["Puertas de Oro", "Totoró, Cauca. Colombia (2022)", ""],
+      ["Casa Refugio", "Popayán, Cauca. Colombia (2022)", ""],
+      ["Monteclaro", "Totoró, Cauca. Colombia (2020)", ""],
+      ["Gran parque de la salud", "Popayán, Cauca. Colombia (2022)", ""],
+      ["Quinta Luna", "Tobia, Cundinamarca. Colombia (2024)", ""],
+      ["Pool house", "Challex, Francia (2024)", ""],
+      ["Chalet", "Challex, Francia (2020)", ""],
+      ["Raccard", "Ayer, Suiza (2024)", ""],
+      ["Hilario", "Colombia (2022)", ""],
+      ["Pintuco", "Colombia (2022)", ""],
+      ["Casa Tertulia", "Popayán, Cauca. Colombia (2021)", ""],
+      ["Spirala", "Portugal, (2020)", "Equipo: Juanita Cuellar, María Barrero + Azuu"],
+      ["Hombre de Maíz", "Honda, Tolima. Colombia (2022)", "Equipo: Maria Ardila, Felipe Langebaek + Azuu"],
+      ["Tomatera", "Victoria, Caldas. Colombia (2023)", "Equipo: Manuela Silva + Azuu"],
+      ["Kurandza Khanimambo", "Mulungo, Mozambique. (2022)", "Equipo: María Rojas, Paula Pinto + Azuu"],
+      ["Casa Roja", "Totoró, Cauca. Colombia (2021)", ""],
+      ["Casa Dorelo", "Totoró, Cauca. Colombia (2021)", ""]
+    ],
+        "about-page-title": "Nosotros",
+        "about-page-description": [
+            "Somos una oficina joven, apasionada por la arquitectura, la artesanía, el diseño y las artes en general. Nuestra visión sobre la arquitectura está profundamente comprometida con la calidad de vida, la innovación, la funcionalidad, los contextos, la belleza, la ética y las emociones. Creemos que la arquitectura es una manifestación de arte capaz de potenciar personas, eventos, emociones, comunidades y sueños. Por ello, imprimimos nuestra energía y dedicación en cada idea, garantizando así, proyectos originales y cautivadores que son resultado de la indagación de los anhelos y sueños de nuestros clientes.",
+            
+            "Asimismo, las energías limpias y renovables son un compromiso en nuestro taller. Pensamos y confiamos en muchas cosas, como el racionamiento de los recursos, en el buen vivir, en el compromiso social de la arquitectura con nuestra gente, nuestra fauna, flora y nuestros paisajes, confiamos en la paz, en nuestra cultura, en la amistad y en el amor. Al final, somos una oficina interesada en comprender diferentes contextos y culturas; por ello, tenemos una proyección global. Nuestra experiencia nos ha permitido desarrollar diversos proyectos residenciales, comerciales, educativos y sanitarios. Lentamente vamos abriendo puertas en diferentes lugares del mundo. Somos, indudablemente, una oficina soñadora"
+        ],
+        "about-page-footer": "Azuu taller de arquitectura. 2024"
+    },
+    en: {
+        "nosotros-link": "About Us",
+        "proyectos-link": "Projects",
+        "carousel-text": [
+      ["Social Hall", "Palacé, Cauca. Colombia (2021)", ""],
+      ["Concha House", "Popayán, Cauca. Colombia (2020)", ""],
+      ["Vivas Courtyard", "Popayán, Cauca. Colombia (2022)", ""],
+      ["Golden Gates", "Totoró, Cauca. Colombia (2022)", ""],
+      ["Refuge House", "Popayán, Cauca. Colombia (2022)", ""],
+      ["Monteclaro", "Totoró, Cauca. Colombia (2020)", ""],
+      ["Grand Health Park", "Popayán, Cauca. Colombia (2022)", ""],
+      ["Quinta Luna", "Tobia, Cundinamarca. Colombia (2024)", ""],
+      ["Pool House", "Challex, France (2024)", ""],
+      ["Chalet", "Challex, France (2020)", ""],
+      ["Raccard", "Ayer, Switzerland (2024)", ""],
+      ["Hilario", "Colombia (2022)", ""],
+      ["Pintuco", "Colombia (2022)", ""],
+      ["Tertulia House", "Popayán, Cauca. Colombia (2021)", ""],
+      ["Spirala", "Portugal, (2020)", "Team: Juanita Cuellar, María Barrero + Azuu"],
+      ["Man of Corn", "Honda, Tolima. Colombia (2022)", "Team: Maria Ardila, Felipe Langebaek + Azuu"],
+      ["Tomatera", "Victoria, Caldas. Colombia (2023)", "Team: Manuela Silva + Azuu"],
+      ["Kurandza Khanimambo", "Mulungo, Mozambique. (2022)", "Team: María Rojas, Paula Pinto + Azuu"],
+      ["Red House", "Totoró, Cauca. Colombia (2021)", ""],
+      ["Dorelo House", "Totoró, Cauca. Colombia (2021)", ""]
+    ],
+        "about-page-title": "About Us",
+        "about-page-description": [
+            "We are a young office, passionate about architecture, craftsmanship, design and the arts in general. Our vision of architecture is deeply committed to quality of life, innovation, functionality, contexts, beauty, ethics and emotions. We believe that architecture is a manifestation of art capable of empowering people, events, emotions, communities and dreams. For this reason, we put our energy and dedication into each idea, guaranteeing original and captivating projects that are the result of the inquiry of our clients' desires and dreams.",
+
+            "Likewise, clean and renewable energies are a commitment in our workshop. We think and trust in many things, such as the rationing of resources, in good living, in the social commitment of architecture with our people, our fauna, flora and landscapes, we trust in peace, in our culture, in friendship and in love. In the end, we are an office interested in understanding different contexts and cultures; therefore, we have a global projection. Our experience has allowed us to develop diverse residential, commercial, educational and health projects. We are slowly opening doors in different parts of the world. We are, undoubtedly, a dreamer's office."
+        ],
+        "about-page-footer": "Azuu architecture workshop. 2024"
+    }
+};
